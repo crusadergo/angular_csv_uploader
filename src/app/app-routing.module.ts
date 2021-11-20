b651f7ef394fa91dcd466c2d6db64f5410dea332
Lazarev Ilia <crusadergo21@gmail.com>
2021-11-20 10:48:21 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CsvListComponent } from './csvList/component';
+import { SignInComponent } from './signIn/component';
+import { SignupComponent } from './signup/component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: CsvListComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'signup',
+        component: SignupComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'signin',
+        component: SignInComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
